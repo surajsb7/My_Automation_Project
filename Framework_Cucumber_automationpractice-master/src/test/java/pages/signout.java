75fe438308e6aee97ac15ea9e39b3d9d20f4af16
SurajBobade <SurajBobade@TORANA-L170.local>
2025-07-23 13:33:23 +0530

    added import

--- Framework_Cucumber_automationpractice-master/src/test/java/pages/signout.java
+++ Framework_Cucumber_automationpractice-master/src/test/java/pages/signout.java
@@ -1,1 +1,12 @@
 package pages;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
